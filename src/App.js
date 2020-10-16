a1124d5f5ee023903917150aa5ab3ebe622838a2
Ostap Pushkar <pushkarostap@gmail.com>
2020-10-17 01:48:01 +0300

    Deleted unused files and lines

--- src/App.js
+++ src/App.js
@@ -1,69 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import Paper from "@material-ui/core/Paper";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Skeleton from "./components/Skeleton";
-import PCB from "./components/PCB";
+import PCB from "./views/PCB";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-prevent-tabpanel-${index}`}
-      aria-labelledby={`scrollable-prevent-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-};
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabIndex: 0,
-    };
-  }
-  handleTab = (event, newValue) => {
-    this.setState({ tabIndex: newValue });
-  };
-
   render() {
     return (
       <div className="App">
         <PCB />
-        {/* <AppBar position="static" color="default">
-          <Tabs
-            value={this.state.tabIndex}
-            indicatorColor="primary"
-            textColor="primary"
-            onChange={this.handleTab}
-          >
-            <Tab label="Skeleton" />
-            <Tab label="PCB" />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={this.state.tabIndex} index={0}>
-          <Skeleton />
-        </TabPanel>
-        <TabPanel value={this.state.tabIndex} index={1}>
-          <PCB />
-        </TabPanel> */}
       </div>
     );
   }
--- src/services.js
+++ src/services.js
@@ -116,7 +116,7 @@ const base = [
     [1, 0, 1],
   ],
 ];
-export const Auth = (c, context, cb) => {
+export const Auth = (c, context, radius, cb) => {
   let imageData = context.getImageData(0, 0, c.width, c.height);
   let data = imageData.data;
   var pixelMatrix = [];
@@ -151,7 +151,7 @@ export const Auth = (c, context, cb) => {
         ) {
           if (i >= 0 && i < 5) {
             context.beginPath();
-            context.arc(x, y, 6, 0, 2 * Math.PI);
+            context.arc(x, y, radius, 0, 2 * Math.PI);
             context.strokeStyle = "red";
             context.lineWidth = 2;
             context.closePath();
@@ -159,7 +159,7 @@ export const Auth = (c, context, cb) => {
             endCenters.push({ x: x, y: y });
           } else {
             context.beginPath();
-            context.arc(x, y, 6, 0, 2 * Math.PI);
+            context.arc(x, y, radius, 0, 2 * Math.PI);
             context.strokeStyle = "blue";
             context.lineWidth = 2;
 
@@ -182,119 +182,4 @@ export const Threshold = (c, context, threshold) => {
     data[i] = data[i + 1] = data[i + 2] = v;
   }
   context.putImageData(imageData, 0, 0);
-};
-
-export const ZhangSuenThinning = (c, context) => {
-  let imageData = context.getImageData(0, 0, c.width, c.height);
-  let data = imageData.data;
-  console.log(data);
-  var pixelMatrix = [];
-  for (let i = 0; i < data.length; i += 4) {
-    pixelMatrix[i / 4] =
-      data[i] < 50 && data[i + 1] < 50 && data[i + 2] < 50 ? 1 : 0;
-  }
-  console.log(pixelMatrix);
-  var newArray = [];
-  while (pixelMatrix.length > 0) {
-    newArray.push(pixelMatrix.splice(0, c.width));
-  }
-  console.log(newArray[0]);
-  var temp = [...newArray];
-  var count = 0;
-  do {
-    count = step(1, temp, newArray);
-    temp = [...newArray];
-    count += step(2, temp, newArray);
-    temp = [...newArray];
-  } while (count > 0);
-  for (let i = 0; i < c.height; i++) {
-    for (let y = 0; y < c.width; y++) {
-      if (newArray[i][y]) {
-        newArray[i][y] = [0, 0, 0, 255];
-      } else {
-        newArray[i][y] = [255, 255, 255, 255];
-      }
-    }
-  }
-  var newData = context.createImageData(c.width, c.height);
-  newData.data.set(newArray.flat(2));
-  context.putImageData(newData, 0, 0);
-};
-
-const step = (stepNo, temp, s) => {
-  var count = 0;
-
-  for (let a = 1; a < temp.length - 1; a++) {
-    for (let b = 1; b < temp[0].length - 1; b++) {
-      if (SuenThinningAlg(a, b, temp, stepNo == 2)) {
-        if (s[a][b]) count++;
-        s[a][b] = 0;
-      }
-    }
-  }
-  return count;
-};
-
-const SuenThinningAlg = (x, y, s, even) => {
-  var p2 = !!s[x][y - 1];
-  var p3 = !!s[x + 1][y - 1];
-  var p4 = !!s[x + 1][y];
-  var p5 = !!s[x + 1][y + 1];
-  var p6 = !!s[x][y + 1];
-  var p7 = !!s[x - 1][y + 1];
-  var p8 = !!s[x - 1][y];
-  var p9 = !!s[x - 1][y - 1];
-
-  var bp1 = NumberOfNonZeroNeighbors(x, y, s);
-  if (bp1 >= 2 && bp1 <= 6) {
-    if (NumberOfZeroToOneTransitionFromP9(x, y, s) == 1) {
-      if (even) {
-        if (p2 === false || p4 === false || p8 === false) {
-          if (p2 == false || p6 == false || p8 == false) {
-            return true;
-          }
-        }
-      } else {
-        if (p2 == false || p4 == false || p6 == false) {
-          if (p4 == false || p6 == false || p8 === false) {
-            return true;
-          }
-        }
-      }
-    }
-  }
-  return false;
-};
-
-const NumberOfZeroToOneTransitionFromP9 = (x, y, s) => {
-  var p2 = !!s[x][y - 1];
-  var p3 = !!s[x + 1][y - 1];
-  var p4 = !!s[x + 1][y];
-  var p5 = !!s[x + 1][y + 1];
-  var p6 = !!s[x][y + 1];
-  var p7 = !!s[x - 1][y + 1];
-  var p8 = !!s[x - 1][y];
-  var p9 = !!s[x - 1][y - 1];
-  var A =
-    +(!p2 && p3) +
-    +(!p3 && p4) +
-    +(!p4 && p5) +
-    +(!p5 && p6) +
-    +(!p6 && p7) +
-    +(!p7 && p8) +
-    +(!p8 && p9) +
-    +(!p9 && p2);
-  return A;
-};
-const NumberOfNonZeroNeighbors = (x, y, s) => {
-  var count = 0;
-  if (s[x - 1][y]) count++;
-  if (s[x - 1][y + 1]) count++;
-  if (s[x - 1][y - 1]) count++;
-  if (s[x][y + 1]) count++;
-  if (s[x][y - 1]) count++;
-  if (s[x + 1][y]) count++;
-  if (s[x + 1][y + 1]) count++;
-  if (s[x + 1][y - 1]) count++;
-  return count;
 };
